2102f1cc46611df974c2ca71b8590e33261f394a
David Neto <dneto@mobiquityinc.com>
2018-10-28 00:38:06 -0400

    added standard intents

---
+++ alexaSkill/handlers/standardHandlers.js
@@ -0,0 +1,99 @@
+// CancelAndStop, SessionEndedRequest, ErrorHandler, Help
+
+const _ = require("lodash")
+
+const LaunchIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'LaunchRequest'
+    },
+    handle(handlerInput) {
+        const speechText = "Welcome to the Pathfinder Spell companion. I provide information on the many arcane abilities found in Golarion. What spell would you like to hear about?"
+        const reprompt = "Ask me about a spell, or I'll whip your ass."
+        return handlerInput.responseBuilder
+                .speak(speechText)
+                .reprompt(reprompt)
+                .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+                || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speechText = "Come back soon, binch!"
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(true)
+            .getResponse();
+    },
+};
+
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+        return handlerInput.responseBuilder
+            .withShouldEndSession(true)
+            .getResponse();
+    },
+};
+
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        console.log(`Error handled: ${error.stack}`);
+        const speechText = "Sorry, there was an error. Please try again later."
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(true)
+            .getResponse();
+    },
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = "This skill tells you about spells. Now, ask me about one."
+        const reprompt = "Are you gonna ask me about a spell?"
+
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(reprompt)
+            .getResponse();
+    },
+};
+
+const FallbackHandler = {
+    canHandle(handlerInput) {
+        return (handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.FallbackIntent')
+            || true
+    },
+    handle(handlerInput) {
+        const speechText = "I'll be honest, that was a very dumb question."
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .withShouldEndSession(true)
+            .getResponse();
+    }
+}
+
+module.exports = {
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    ErrorHandler,
+    HelpIntentHandler,
+    LaunchIntentHandler,
+    FallbackHandler
+}
