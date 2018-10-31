c38719bcd183b4d1bc812c1d24760e2b84ba5c7a
David Neto <dneto@mobiquityinc.com>
2018-10-31 14:58:24 -0400

    spellHandler can now access dynamo

--- alexaSkill/handlers/spellHandlers.js
+++ alexaSkill/handlers/spellHandlers.js
@@ -1,6 +1,6 @@
 var AWS = require('aws-sdk');
 AWS.config.update({region: 'us-east-1'});
-// var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
+var dynamodb = new AWS.DynamoDB();
 var docClient = new AWS.DynamoDB.DocumentClient();
 
 
@@ -9,32 +9,34 @@ const SpellIntentHandler = {
         return (handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
             handlerInput.requestEnvelope.request.intent.name === 'SpellIntent')
     },
-    async handle(handlerInput) {
-        const speechText = "Eventually this intent will tell you about the provided spell."
+    handle(handlerInput) {
+        return new Promise((resolve) => {
+            params = {
+                TableName: "pathfinderSpellsTable",
+                Key: {
+                    "id": 23
+                }
+            };
 
-        params = {
-            TableName: "pathfinderSpellsTable",
-            Key: {
-                "id": 23
-            }
-        };
-        
-        await docClient.get(params, function(err, data) {
-            if (err) {
-                console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
-            } else {
-                console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
-                return handlerInput.responseBuilder
-                    .speak(`here is your data: ${data.Item.name}`)
-                    .withShouldEndSession(true)
-                    .getResponse();
-            }
-        });
-        
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .withShouldEndSession(true)
-            .getResponse();
+            docClient.get(params, function(err, data) {
+                if (err) {
+                    console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+                } else {
+                    if (data.Item) {
+                        return resolve(handlerInput.responseBuilder
+                            .speak(`Your spell is ${data.Item.name}`)
+                            .withShouldEndSession(true)
+                            .getResponse());
+                    } else {
+                        return resolve(handlerInput.responseBuilder
+                            .speak(`I couldn't find that spell.`)
+                            .withShouldEndSession(true)
+                            .getResponse());
+                    }
+                }
+            });
+            
+        })
     }
 }
 
--- alexaSkill/test/SpellHandler.test.js
+++ alexaSkill/test/SpellHandler.test.js
@@ -7,7 +7,7 @@ describe("Enter SpellIntent", () => {
     describe("returns dummy message", () => {
         alexaTest.test([{
             request: alexaTest.getIntentRequest("SpellIntent"),
-            saysLike: languageStrings.en.SPELL_ASK,
+            saysLike: languageStrings.en.translation.SPELL_ASK,
             shouldEndSession: true
         }]);
     });
