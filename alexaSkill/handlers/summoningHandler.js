7c2150710b6a1d58740b23c483af3f5a6a9eaa67
David Neto <dneto@mobiquityinc.com>
2018-12-13 08:48:42 -0500

    summon handler work

--- alexaSkill/handlers/summoningHandler.js
+++ alexaSkill/handlers/summoningHandler.js
@@ -1,20 +1,24 @@
 const helper = require("../services/helperFunctions.js");
 const summoningData = require("../resources/summoningData.json").spells
 
+function checkIfSummoning(spell) {
+    // this spell should check if the spell being asked about is
+}
+
 const SummoningIntentHandler = {
     canHandle(handlerInput) {
         return (handlerInput.requestEnvelope.request.type === 'IntentRequest' &&
             handlerInput.requestEnvelope.request.intent.name === 'SummoningIntent');
     },
     handle(handlerInput) {
-        let speechText
-        let reprompt
+        let speechText;
+        let reprompt;
 		const requestAttributes = handlerInput.attributesManager.getRequestAttributes();
 		const sessionAttributes = handlerInput.attributesManager.getSessionAttributes();
-		const slot = helper.slotValue(handlerInput.requestEnvelope.request.intent.slots.modifier)
+		const slot = helper.slotValue(handlerInput.requestEnvelope.request.intent.slots.modifier);
 
-        // if slot isn't empty && matches a viable summoning spell, 
-        if (slot !== "" && matches a viable slot) {
+        // if slot matches a viable summoning spell OR spell in sessionAttributes matches
+        if (`slot matches a viable slot || sessionAttributes.currentSpell != ""`) {
 			speechText = requestAttributes.t("SUMMON_FOUND")
 			reprompt = requestAttributes.t("SUMMON_FOUND_REPROMPT")
 		} else {
@@ -25,7 +29,7 @@ const SummoningIntentHandler = {
         return handlerInput.responseBuilder
             .speak(speechText)
             .reprompt(reprompt)
-            .getResponse();
+            .getResponse()
     }
 };
 
